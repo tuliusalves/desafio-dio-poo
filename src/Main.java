f4a1f4cfa904652ea52b8852035da3af3c4fb5b1
Túlius <tulius_sr@outlook.com>
2023-01-09 16:17:04 -0300

    Adicionando mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("Descrição curso java");
+        curso1.setCargaHoraria(8);
+        System.out.println(curso1);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("mentoria de java");
+        mentoria1.setDescricao("descrição mentoria java");
+        mentoria1.setData(LocalDate.now());
+        System.out.println(mentoria1);
     }
 }
